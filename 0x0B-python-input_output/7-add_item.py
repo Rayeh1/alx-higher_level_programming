3a08f9aeaf7ee91a2d6a04c30d1d8e5c3d8edd1c
Rayeh1 <113691513+Rayeh1@users.noreply.github.com>
2023-02-09 01:21:25 +0000

    auto

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""
+Writes a script that adds all arguments from cmd line to a Python list
+then saves to a JSON file.
+"""
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+
+filename = "add_item.json"
+
+
+try:
+    json_list = load_file(filename)
+except FileNotFoundError:
+    json_list = []
+
+
+json_list += sys.argv[1:]  # append items to list
+save_file(json_list, filename)  # save items to file
